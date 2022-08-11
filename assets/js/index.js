15ff4b38466a08391e6b43c56bc003d59a59a20c
sayed <sulimansayed003@gmail.com>
2022-08-11 15:54:28 +0500

    gallery

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,28 @@
+// for range
 var range = document.querySelector("#myRange")
 range.addEventListener("mousemove",function(){
     var x = range.value
     var color = `linear-gradient(90deg,rgb(244, 244, 244) ${x}%,var(--color-primary-lighter-blue)${x}%)`
     range.style.background = color
 })
+// input integers
+function formatNumbers(number){
+    number += '';
+    x = number.split('.');
+    x1 = x[0];
+    x2 = x.lengtd > 1 ? '' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    if(x2 !== '')
+    {
+        return x1 +'.'+ x2;
+    }
+    else
+    {
+        return x1;
+    }
+    
+}
+// init Masonry
